6b5a263de54873b4be9e1137411cd4d05cc18ac4
Ben <bvdhest1998@gmail.com>
2021-03-31 19:40:27 -0700

    added to post feature

--- js/wall.js
+++ js/wall.js
@@ -1,79 +1,54 @@
-$(function(){
-    let postDiv = $("#post-div");
-    let makePostDiv = $("#make-post-div");
-    let postBttn = $("#post-bttn").click(function(){
-        let textArea = '<textarea id="w3review" name="w3review" rows="4" cols="50"></textarea>';
-        makePostDiv.append(textArea);
+$(function() {
+  let postDiv = $("#post-div");
+  let makePostDiv = $("#make-post-div");
+  let newPostBttn = $("#new-post-bttn").click(function() {
+    $.fn.HideNewPostButton();
+    let textArea = $(
+      '<textarea id="walltextarea" class="mt-4" rows="4" cols="50"></textarea>'
+    );
+    let postBttn = $('<button type="button">Post</button>');
+    postBttn.click(function() {
+      $.fn.PostToWall();
     });
-})
+    let postBttnDiv = $("<div></div>");
+    postBttnDiv.append(postBttn);
+    makePostDiv.append(textArea, postBttnDiv);
+  });
 
-/*let postDiv = document.getElementById("post-div");
-let makePostDiv = document.getElementById("make-post-div");
-let postBttn = document.getElementById("post-bttn");
+  $.fn.HideNewPostButton = function() {
+    $("#new-post-bttn").hide();
+  };
 
-postBttn.addEventListener("click", function() {
-  CreatePostUI();
+  $.fn.PostToWall = function() {
+    let textArea = $("#walltextarea");
+    let postContent = $(`<p id="postContent"></p>`);
+    let newPostDiv = $("<div></div>");
+    //username which will be displayed in post, set to: test for now
+    let userName = ($("<p></p>").innerHTML = "test");
+    let userNameDiv = $(`<div></div>`);
+    userNameDiv.append(userName);
+
+    //date that will be displayed in post
+    let d = new Date();
+    let strDate =
+      d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate();
+    let dateParagraph = ($("<p></p>").innerHTML = strDate);
+    let dateParagraphDiv = $('<div class="ml-4"></div>');
+    dateParagraphDiv.append(dateParagraph);
+
+    let topDiv = $(`<div class="mt-4"></div>`).append(
+      userNameDiv,
+      dateParagraphDiv
+    );
+    topDiv.attr("id", "top-div");
+    //set the inner html of the postContent paragraph to what user typed in text area
+    postContent.html(textArea.val());
+    //check first that the text area isnt empty
+    if (textArea.val() != "") {
+      //clear the text area so the user can make a new post
+      textArea.val("");
+      newPostDiv.append(topDiv, postContent);
+      postDiv.append(newPostDiv);
+    }
+  };
 });
-
-function CreatePostUI() {
-  RemoveNewPostBttn();
-  let textarea = document.createElement("textarea");
-  Object.assign(textarea, {
-    rows: "4",
-    cols: "50",
-    className: "mt-4",
-    id: "walltextarea",
-  });
-  makePostDiv.appendChild(textarea);
-  let postBttn = document.createElement("button");
-  Object.assign(postBttn, {
-    className: "mt-4",
-    innerHTML: "Post",
-  });
-  postBttn.addEventListener("click", function() {
-    PostToWall();
-  });
-  let postBttnDiv = document.createElement("div");
-  postBttnDiv.appendChild(postBttn);
-  makePostDiv.appendChild(postBttnDiv);
-}
-
-function RemoveNewPostBttn() {
-  postBttn.parentNode.removeChild(postBttn);
-}
-
-$(function PostToWall() {
-  let textArea = $("#walltextarea");
-  let postContent = $("<p></p>");
-  let newPostDiv = $("<div></div>");
-  let userName = ($("<p></p>").innerHTML = "test");
-
-  let d = new Date();
-  let strDate = d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate();
-  let dateParagraph = ($("<p></p>").innerHTML = strDate);
-
-  let topDiv = $("<div></div>").appendChild(userName,dateParagraph);
-  topDiv.attr('id','top-div');
-
-  postContent.innerHTML = textArea.value;
-  newPostDiv.appendChild(topDiv,postContent);
-  postDiv.appendChild(newPostDiv);
-});
-
-/*function PostToWall() {
-  let textArea = document.getElementById("walltextarea");
-  let postContent = document.createElement("p");
-  let newPostDiv = document.createElement("div");
-
-  let userName = document.createElement("p");
-  userName.innerHTML = "test";
-  let dateParagraph = document.createElement("p");
-  dateParagraph.innerHTML = "" + Date;
-  let topDiv = document.createElement("div");
-  topDiv.appendChild(userName);
-  topDiv.appendChild(dateParagraph);
-  postContent.innerHTML = textArea.value;
-  newPostDiv.appendChild(topDiv);
-  newPostDiv.appendChild(postContent);
-  postDiv.appendChild(newPostDiv);
-}*/

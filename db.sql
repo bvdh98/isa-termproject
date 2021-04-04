c7aeec23b46b585dafb30728d02913e53d470fe3
Ben <bvdhest1998@gmail.com>
2021-04-03 18:45:02 -0700

    added wall posts table to db

--- db.sql
+++ db.sql
@@ -14,3 +14,21 @@ CREATE TABLE USERS(
 CREATE TABLE POSTS(
   content VARCHAR(255)
 );
+
+/*
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `wall_posts` (
+  `wall_post_id` int NOT NULL AUTO_INCREMENT,
+  `text` varchar(45) NOT NULL,
+  `date` varchar(45) NOT NULL,
+  `users_id` int NOT NULL,
+  PRIMARY KEY (`wall_post_id`),
+  KEY `fk_wall_posts_users_idx` (`users_id`),
+  CONSTRAINT `fk_wall_posts_users` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+*/

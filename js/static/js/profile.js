077d8509364b668286c5dd3bf79a82c3eec50d49
Ben <bvdhest1998@gmail.com>
2021-04-10 20:28:13 -0700

    added back logout feature on profile

--- js/static/js/profile.js
+++ js/static/js/profile.js
@@ -1,7 +1,7 @@
 $(function() {
-    $("#wall-bttn").click(function(){
-        GoToWall();
-    });
+  $("#wall-bttn").click(function() {
+    GoToWall();
+  });
   (async () => {
     user = await GetProfile();
     $.fn.LoadProfile(user);
@@ -10,18 +10,18 @@ $(function() {
   $.fn.LoadProfile = function(user) {
     let profileDiv = $("#profile-div");
     if (user.displayName == null && user.about == null) {
-        let form = `<form id="profile-form">
+      let form = `<form id="profile-form">
                         <label for="name">name:</label><br>
                         <input type="text" id="name" name="name" required><br><br>
                         <label for="profile-textarea">About you: </label><br>
                         <textarea id="profile-textarea" name="textarea" rows="4"
                             cols="50">Tell us about yourself</textarea><br><br>
                     </form>
-                    <button id="profile-save-bttn" type="submit">Save</button><br><br>`
-        profileDiv.append(form);
-        $("#profile-save-bttn").click(function() {
-            SaveProfile($("#name").val(),$("#profile-textarea").val());
-        });
+                    <button id="profile-save-bttn" type="submit">Save</button><br><br>`;
+      profileDiv.append(form);
+      $("#profile-save-bttn").click(function() {
+        SaveProfile($("#name").val(), $("#profile-textarea").val());
+      });
     }
     let displayNameDiv = document.createElement("div");
     let nameParagraph = document.createElement("p");
@@ -35,23 +35,45 @@ $(function() {
     aboutParagraph.innerHTML = user.about;
     aboutParagraph.className = "mt-4";
 
-    profileDiv.append(displayNameDiv,aboutHeader,aboutParagraph);
+    profileDiv.append(displayNameDiv, aboutHeader, aboutParagraph);
   };
 });
 
 const domainURL = "http://localhost:8888";
 let endPoint = "/walls/API/V1";
 
-//domainURL.concat(endPoint, "/post/") + id
+GoToWall = async function() {
+  const response = await fetch(domainURL.concat("/wall"));
+  if (response.status == 200) {
+    window.location.assign(domainURL.concat("/wall"));
+  }
+};
 
-GoToWall = async function(){
-    const response = await fetch(domainURL.concat("/wall"));
-    if(response.status == 200) {
-        window.location.assign(domainURL.concat("/wall"));
-    }
-}
-//http://localhost:8888/walls/API/V1/user/profile
-SaveProfile = async function(name,about) {
+LogOut = async function() {
+  const getMethod = {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  };
+
+  const response = await fetch(
+    domainURL.concat(endPoint, "/user/logout"),
+    getMethod
+  );
+  if (response.status == 200) {
+    alert("Logout Successful");
+    window.location.assign(domainURL);
+  }
+};
+
+let logoutBtn = document.getElementById("logoutBtn");
+
+logoutBtn.addEventListener("click", function() {
+  LogOut();
+});
+
+SaveProfile = async function(name, about) {
   let profile = {};
   console.log(document.getElementById("name").innerHTML);
   profile.displayName = name;
@@ -65,7 +87,7 @@ SaveProfile = async function(name,about) {
     body: JSON.stringify(profile),
   };
   const response = await fetch(
-    domainURL.concat(endPoint,"/user/profile"),
+    domainURL.concat(endPoint, "/user/profile"),
     putMethod
   );
   if (response.status == 200) {
@@ -76,7 +98,7 @@ SaveProfile = async function(name,about) {
 };
 
 GetProfile = async function() {
-  const response = await fetch(domainURL.concat(endPoint,"/user/profile"));
+  const response = await fetch(domainURL.concat(endPoint, "/user/profile"));
   const userNodeList = await response.json();
   for (let i = 0; i < userNodeList.results.length; i++) {
     let user = JSON.parse(userNodeList.results[i]);
